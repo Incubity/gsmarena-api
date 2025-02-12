95f07b54e5d5b7e965231f4f66053fbea0f12f64
Donovan Charpin <Donovan.charpin@gmail.com>
2025-02-12 10:56:43 -0500

    Added free search

--- src/services/search.js
+++ src/services/search.js
@@ -22,6 +22,28 @@ const search = async (searchValue) => {
     return json;
 };
 
+const freeSearch = async (searchValue) => {
+    const html = await getDataFromUrl(`/results.php3?sFreeSearch=yes&sFreeText=${searchValue}`);
+
+    const $ = cheerio.load(html);
+    const json = [];
+
+    const devices = $('.makers').find('li');
+    devices.each((i, el) => {
+        const imgBlock = $(el).find('img');
+        json.push({
+            id: $(el).find('a').attr('href').replace('.php', ''),
+            name: $(el).find('span').html().split('<br>')
+                .join(' '),
+            img: imgBlock.attr('src'),
+            description: imgBlock.attr('title'),
+        });
+    });
+
+    return json;
+};
+
 module.exports = {
     search,
+    freeSearch,
 };
